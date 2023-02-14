ffa46b1ebc44e1f45c1525c87946aa7d5e773f28
Aryan salge <aryansalge02@gmail.com>
2023-02-14 23:45:09 +0530

    Add files via upload

---
+++ challenge.py
@@ -0,0 +1,26 @@
+#celsius to ferenhit
+'''
+c=input("enter celcius: ")
+c=float(c)
+f=(c * 9/5)+32
+print("farhenhit : " ,f)
+'''
+'''
+#farhenit to celsius
+f=input("farhenit: ")
+f=float(f)
+c=(f-32) * 5/9
+print("celsius: ",c)
+'''
+'''
+#Simple intrest calculator
+#eqn: pnr/100
+p=input("p: ")
+n=input("n: ")
+r=input("r: ")
+p=int(p)
+n=int(n)
+r=int(r)
+si=(p*n*r)/100
+print("simple intrest :",si)
+'''
---
+++ elif.py
@@ -0,0 +1,33 @@
+
+#elif
+'''
+op_1=int(input("enter number 1: "))
+op_2=int(input("enter number 2: "))
+operator=input("enter operator (+,-,*,/, **):")
+#if (test exp):
+    #statement1
+#elif (test exp);
+    #statement2
+#....(you can put as many elif as you want)
+#else:
+     #statement3
+
+if operator== '+':
+    print(op_1 + op_2)
+elif operator=='-':
+    print(op_1 - op_2)
+elif operator == '*':
+    print(op_1 * op_2)
+elif operator=='/':
+    print(op_1 / op_2)
+elif operator=='**':
+    print(op_1 ** op_2)
+else:
+    print("none")
+'''
+
+#range
+'''
+n=list(range(2,9))
+print(n)
+'''
